25087aeed6eb40e259629f7d3c0483df9bededb0
andylegrand <andyleg@uw.edu>
2026-01-03 14:19:23 -0800

    removed bad import

--- core/tab_table.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-class TabTable:
-    def __init__(self,table_vals):
-        self.table_vals = table_vals
-        self.drug_names = None
-        self.cuboids_count = None
-        self.is_background = None
-
