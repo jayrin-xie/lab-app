f6b56c83db1bea2c552b6530b3d757cf15919d2d
Jayrin Xie <jayrinxie@Jayrins-MacBook-Pro.local>
2026-01-03 00:06:55 -0600

    added tab_table

---
+++ core/tab_table.py
@@ -0,0 +1,8 @@
+import numpy as np
+class TabTable:
+    def __init__(self,table_vals):
+        self.table_vals = table_vals
+        self.drug_names = None
+        self.cuboids_count = None
+        self.is_background = None
+
